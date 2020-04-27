fec6ead82cea947e30d7bef12cbfd855c5bfb644
sweatysock <mark@electricgt.co>
2020-04-27 12:04:54 +0100

    Getting http listening sorted

--- server.js
+++ server.js
@@ -103,36 +103,22 @@ function applyAutoGain(audio, startGain) {		// Auto gain control
 }
 
 // Network code
-var SSLPORT = 443; //Default 443
-var HTTPPORT = process.env.PORT; //Default 80 (Only used to redirect to SSL port)
-var privateKeyPath = "./cert/key.pem"; //Default "./cert/key.pem"
-var certificatePath = "./cert/cert.pem"; //Default "./cert/cert.pem"
+var PORT = process.env.PORT; 
 
 var fs = require('fs');
 var express = require('express');
-var https = require('https');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-var privateKey = fs.readFileSync( privateKeyPath );
-var certificate = fs.readFileSync( certificatePath );
-
-//var server = https.createServer({
-//    key: privateKey,
-//    cert: certificate
-//}, app).listen(SSLPORT);
-
-// Redirect from http to https
 var http = require('http');
-http.createServer(function (req, app) {
-//    res.writeHead(301, { "Location": "https://" + req.headers['host'] + ":"+ SSLPORT + "" + req.url });
-//    res.end();
-}).listen(HTTPPORT);
+var server = http.Server(app);
+server.listen(HTTPPORT, function() {
+	console.log("Server running on ",HTTPPORT);
+});
 
-var io  = require('socket.io').listen(http, { log: false });
+var io  = require('socket.io').listen(server, { log: false });
 
-console.log("Webserver & Socketserver running on port: "+SSLPORT+ " and "+ HTTPPORT);
 
 
 
@@ -270,3 +256,4 @@ function generateMix () {
 
 // We are all set up so let the idling begin!
 enterState( idleState );
+console.log("IDLING...");
