3e57b86545821c71e9086cad11d25e60d9b6133a
sweatysock <mark@electricgt.co>
2020-05-04 18:35:34 +0100

    Feedback issues... not working with just a flag change? strange...

--- public/js/main.js
+++ public/js/main.js
@@ -77,7 +77,7 @@ function startTalking() { 				// Get mic access and connect it up
 		console.log('getUserMedia supported.');
 		var constraints = {
 			mandatory: {
-				googEchoCancellation: true,
+				googEchoCancellation: false,
 				googAutoGainControl: false,
 				googNoiseSuppression: false,
 				googHighpassFilter: false
