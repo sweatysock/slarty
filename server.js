b55728bb81335a0e172576fcc4d1beeb5df2c5bd
sweatysock <mark@electricgt.co>
2020-05-25 19:53:15 +0100

    Server error when existing client tries to reconnect

--- server.js
+++ server.js
@@ -148,6 +148,7 @@ io.sockets.on('connection', function (socket) {
 		let requestedChannel = data.channel;			// If a reconnect they will already have a channel
 		let channel = -1;					// Assigned channel. -1 means none (default response)
 console.log(data);
+console.log(channels[requestedChannel]);
 		if ((requestedChannel != -1) &&	(channels[requestedChannel].socketID === undefined)) {
 			channel = requestedChannel;			// If requested channel is set and available reassign it
 		} else {
