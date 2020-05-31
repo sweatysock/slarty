abdab2152f9517de014c8104fcb76e08886110a1
sweatysock <mark@electricgt.co>
2020-05-31 21:55:51 +0100

    Investigating gating on mic input. Mistyped variable name

--- public/js/main.js
+++ public/js/main.js
@@ -354,7 +354,7 @@ function processAudio(e) {						// Main processing loop
 			let outAudio = micBuffer.splice(0, PacketSize);	// Get a packet of audio
 			let floor = maxValue(outAudio);			// Get peak level for this packet
 trace2("floor: ",floor);
-			if (floor > mic.threshold) {			// if audio level is above threshold send it
+			if (floor > micIn.threshold) {			// if audio level is above threshold send it
 trace2("good enough");
 				let obj = applyAutoGain(outAudio, micIn.gain, 5);	// Bring the mic up to level, but 5x is max
 				if (obj.peak > micIn.peak) micIn.peak = obj.peak;	// Note peak for local display
