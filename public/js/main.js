6425719f8b7d6bf1447678805c33488768443e9d
sweatysock <mark@electricgt.co>
2020-06-02 21:32:59 +0100

    Back to gating as best noise control strategy. Added gate closure delay plus fade down and fade up to smooth cosing and reopening transitions. However, sounds awful!

--- public/js/main.js
+++ public/js/main.js
@@ -41,7 +41,7 @@ var micIn = {								// and for microphone input
 	muted	: false,
 	peak	: 0,
 	channel	: "micIn",
-	threshold:0.005,						// Level below which we don't send audio
+	threshold:0.000,						// Level below which we don't send audio
 	gate	: 0,							// Threshold gate. >0 means open.
 };
 
