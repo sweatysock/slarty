0358e098e4adef74c3a79e4679b8ac62d7375ddc
sweatysock <mark@electricgt.co>
2020-06-09 15:05:50 +0100

    Seems good now. Chrome still has some audio issues, but getting better. Trying without threshold again...

--- public/js/main.js
+++ public/js/main.js
@@ -920,7 +920,7 @@ function printReport() {
 	let state = "Green";
 	trace("micIn.peak: ",micIn.peak.toFixed(1)," micIn.gain: ",micIn.gain.toFixed(1)," mixOut.peak: ",mixOut.peak.toFixed(1)," mixOut.gain: ",mixOut.gain.toFixed(1));
 	trace("Levels of output: ",levelCategories);
-	setNoiseThreshold();						// Set mic noise threshold based on level categories
+//	setNoiseThreshold();						// Set mic noise threshold based on level categories
 	if ((overflows > 1) || (shortages >1)) state = "Orange";
 	if (socketConnected == false) state = "Red";
 	setStatusLED("GeneralStatus",state);
