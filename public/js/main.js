d01977e808a2399b155a6904a9f9d9e2cd52164f
sweatysock <mark@electricgt.co>
2020-06-09 19:24:44 +0100

    Mac threshold too low again... adding traces

--- public/js/main.js
+++ public/js/main.js
@@ -592,7 +592,7 @@ function processAudio(e) {						// Main processing loop
 		thresholdBuffer[echoTest.sampleDelay],	
 		thresholdBuffer[echoTest.sampleDelay+1]
 	])) * echoTest.factor * micIn.gain;				// Apply most aggressive threshold of current +/-1
-trace2("thresh ",micIn.threshold," gain ",micIn.gain);
+trace2("thresh ",micIn.threshold," gain ",micIn.gain," max ",max);
 	thresholdBuffer.pop();						// Remove oldest threshold buffer value
 	let spkrAudio = upSample(outAudio, SampleRate, soundcardSampleRate); // Bring back to HW sampling rate
 	for (let i in outData) 
