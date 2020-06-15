58b84a573ca0939925a59bbd5534c1cdfa5be4ab
sweatysock <mark@electricgt.co>
2020-06-15 14:16:12 +0100

    Adding mid boost filter to server. Built it. Now testing by adding to client for trials - All processing removed.. testing mic Filter with new params

--- public/js/main.js
+++ public/js/main.js
@@ -628,9 +628,8 @@ function handleAudio(stream) {						// We have obtained media access
 	let splitter = context.createChannelSplitter(2);		// Split signal for echo cancelling
 
 	// Time to connect everything...
-//	liveSource.connect(micFilter);					// Mic goes to micFilter
-//	micFilter.connect(node);					// micFilter goes to audio processor
-liveSource.connect(node);	
+	liveSource.connect(micFilter);					// Mic goes to micFilter
+	micFilter.connect(node);					// micFilter goes to audio processor
 	node.connect(splitter);						// our processor feeds to a splitter
 	splitter.connect(context.destination,0);			// other output goes to speaker
 
