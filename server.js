4b7397964d2e539c44809a64e41736fd2f58f31d
sweatysock <mark@electricgt.co>
2020-07-08 13:21:48 +0100

    Performer audio still testing. Normal audio not returning on downstream server. Tracing...

--- server.js
+++ server.js
@@ -377,8 +377,8 @@ function midBoostFilter(audioIn) {					// Filter to boost mids giving distant so
 		out1[i] = (out1[i-1] + audioIn[i] - audioIn[i-1]) * alpha;
 	prevFilt1In = audioIn[audioIn.length-1];			// Save last input sample for next filter loop
 	prevFilt1Out = out1[out1.length-1];				// and last output sample for same reason
-if (prevFilt1In.isNaN()) console.log(audioIn);
-if (prevFilt1Out.isNaN()) console.log(audioIn);
+if (isNaN(prevFilt1In)) console.log(audioIn);
+if (isNaN(prevFilt1Out)) console.log(audioIn);
 	return out1;							// Testing with just the high pass filter
 }
 
