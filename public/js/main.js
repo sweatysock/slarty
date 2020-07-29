ac7a14e37b09475121e129730f18075614e9faac
sweatysock <mark@electricgt.co>
2020-07-29 17:50:14 +0100

    No audio being output. Tracing server. Error fixed in client and in server. Now infinite loop in client... is it the while?

--- public/js/main.js
+++ public/js/main.js
@@ -133,6 +133,8 @@ console.log("scanning for our packet in the packet buffer");
 				let p = packetBuf.shift();		// Remove the oldest packet from the buffer
 console.log("p.sequence = ",p.sequence," s = ",s);
 				if (p.sequence == s) {			// We have found the right sequence number
+console.log("p.audio.length = ",p.audio.length);
+console.log(mix);
 					let a = p.audio;		// Get packet's audio, apply same gain as server applied & subtract from mix
 					for (let i=0; i < a.length; p++) mix[i] = mix[i] - a[i] * venueGain;
 					break;				// Packet found. Stop scanning the packet buffer. 
