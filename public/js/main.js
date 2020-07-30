3d00c2e6106d211995479d86f063d87214565dd4
sweatysock <mark@electricgt.co>
2020-07-30 13:50:24 +0100

    Still getting echo.... tracing again

--- public/js/main.js
+++ public/js/main.js
@@ -124,6 +124,7 @@ socketIO.on('d', function (data) {
 		let venueGain = 0;					// Default venue gain in case there was no channel 0 audio
 		if (data.channels[0] != null) {				// If there is venue audio (can't take it for granted)
 			venueGain = data.channels[0].gain;		// Channel 0's mix has had this gain applied to all its' channels
+console.log("venue gain = ",venueGain);
 			let s = data.channels[0].seqNos[myChannel];	// Channel 0's mix contains our audio. This is its sequence no.
 			if (s == null)
 				trace("No sequence number for our audio in mix");
@@ -132,6 +133,8 @@ socketIO.on('d', function (data) {
 					let p = packetBuf.shift();	// Remove the oldest packet from the buffer
 					if (p.sequence == s) {		// We have found the right sequence number
 						let a = p.audio;	// Fill mix with my inverted level-corrected audio
+console.log("my packet in buffer:");
+console.log(a);
 						for (let i=0; i < a.length; i++) mix[i] =  -1 * a[i] * venueGain;
 						break;			// Packet found. Stop scanning the packet buffer. 
 					}
@@ -141,6 +144,10 @@ socketIO.on('d', function (data) {
 		// 2. Build a mix of all incoming channels. For individuals this is just channel 0, For groups it is more
 		data.channels.forEach(c => {				// Process all audio channel packets sent from server
 			let ch = c.channel;				// Channel number the packet belongs to
+if (ch == 0) {
+console.log("channel 0 audio...");
+console.log(c.audio);
+}
 			let chan = channels[ch];			// Local data structure for this channel
 			if (c.socketID != socketIO.id) {		// Don't include my audio in mix
 				chan.name = c.name;			// Update local structure's channel name
