2728999fdd696a7d9ec5aa798eaf71d805220086
sweatysock <mark@electricgt.co>
2020-09-06 09:36:58 +0100

    Not behaving well... investigating with traces...

--- server.js
+++ server.js
@@ -364,6 +364,7 @@ io.sockets.on('connection', function (socket) {
 });
 
 function packetBad(p) {							// Perform basic checks on client packet to stop some hacks
+console.log(p);
 	if (p.audio.mono8 === undefined) return false;
 	if (p.audio.mono16 === undefined) return false;
 	if (p.audio.mono32 === undefined) return false;
