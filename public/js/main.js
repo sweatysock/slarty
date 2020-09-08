099ffdbd76f2c7f047ed96218bdb3f7da6fa9acd
sweatysock <mark@electricgt.co>
2020-09-08 18:43:23 +0100

    Stereo coming through. It IS stereo! :) Cooooool. Now trying lower BW stereo. It still sounds good. Not AS good, but man, close!

--- public/js/main.js
+++ public/js/main.js
@@ -871,6 +871,7 @@ function processAudio(e) {						// Main processing loop
 			}
 audio.stereo32=[];
 audio.stereo16=[];
+audio.mono32=[];
 			let sr = performer ? PerfSampleRate : SampleRate;
 			let now = new Date().getTime();
 			let packet = {
