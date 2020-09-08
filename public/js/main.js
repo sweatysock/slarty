cc9009fd9b4630f02fdf8df4000f04971981503c
sweatysock <mark@electricgt.co>
2020-09-08 19:00:13 +0100

    Confirmed. mono 8kHz is ok if that's all there is. Next step up is 16kHz mono. Correct. Now with 8kHz stereo for next level. And next go to 32kHz mono keeping stereo at 8kHz. Very good. Now stereo at 16kHz

--- public/js/main.js
+++ public/js/main.js
@@ -870,7 +870,6 @@ function processAudio(e) {						// Main processing loop
 				audio = {mono8,mono16,mono32,stereo8,stereo16,stereo32};	
 			}
 audio.stereo32=[];
-audio.stereo16=[];
 			let sr = performer ? PerfSampleRate : SampleRate;
 			let now = new Date().getTime();
 			let packet = {
