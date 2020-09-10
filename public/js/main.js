9cd8e399b03494db5787a10a7d2922f6c0973d9c
sweatysock <mark@electricgt.co>
2020-09-10 23:24:46 +0100

    No have reverb working properly and loading IR files! Yeah!!! Now finding the right acoustic for starting off

--- public/js/main.js
+++ public/js/main.js
@@ -1071,7 +1071,7 @@ function handleAudio(stream) {						// We have obtained media access
 	let combiDelayR = context.createChannelMerger();
 	let delayL = context.createDelay();
 	let delayR = context.createDelay();
-	delayL.delayTime.value = 0.0009;
+	delayL.delayTime.value = 0.0019;
 	delayR.delayTime.value = 0.0005;
 
 	liveSource.connect(micFilter1);					// Mic goes to the lowpass filter
