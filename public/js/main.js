4b70c7c2ae62edc109c6914ebbb968d57e8be1d0
sweatysock <mark@electricgt.co>
2020-09-10 13:54:47 +0100

    Having the usual channel issues, but the audi context jsut stops working and gives no errors! Trying with a simple stereo reverb node in series first

--- public/js/main.js
+++ public/js/main.js
@@ -1059,7 +1059,8 @@ function handleAudio(stream) {						// We have obtained media access
 	liveSource.connect(micFilter1);					// Mic goes to the lowpass filter
 	micFilter1.connect(micFilter2);					// then to the highpass filter
 	micFilter2.connect(node);					// then to the node where all the work is done
-	node.connect(combiner);
+	node.connect(combiner,0,0);
+	node.connect(combiner,1,1);
 	combiner.connect(context.destination);
 
 	startEchoTest();
