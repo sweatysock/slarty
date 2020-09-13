0edbc91115cdd6520934e51c0de54756eab84176
sweatysock <mark@electricgt.co>
2020-09-13 16:42:33 +0100

    Stereo group code built. Now testing...

--- public/js/main.js
+++ public/js/main.js
@@ -222,7 +222,7 @@ v = v8;
 					let d = groupLayout[p];		// Get the delay (in samples @8kHz) for this position
 					d = (d + 18-(myDelay+1)) % 18;	// Adjust the delay relative to my position
 					d = d - 8;			// Delays are offset by 8MARK
-if (tracecount > 0) console.log("delay for channel ",ch," at position ",p," is now ",d,"mS");
+if (tracecount > 0) console.log("delay for channel ",ch," at position ",p," is now ",d," samples");
 					let g = (chan.agc 		// Apply gain. If AGC use mix gain, else channel gain
 						? mixOut.gain : chan.gain);	
 					chan.gain = g;			// Channel gain level should reflect gain applied here
@@ -231,14 +231,13 @@ if (tracecount > 0) console.log("delay for channel ",ch," at position ",p," is n
 						let dl = 0, dr = 0;	// Delay offsets for each channel. Default is no offset
 						if (d < 0) {		// Apply delay to right channel
 							dr = d * -1;	// Invert delay 
-if (tracecount > 0) console.log("delaying ",dr," samples on right channel. Mono8 buffer for this channel is",b8);
 							if (b8 != [])			// If there are samples in the delay buffer
-								R8.splice(0,b8.length,...b8);
+								R8.splice(0,b8.length,b8);
 							chan.buffer8 = m8.slice(m8.length-dr,dr);	// & copy final samples to delay buffer
 						} else {				// Apply delay to left channel
 							dl = d;		
 							if (b8 != [])			// Same as for right channel
-								L8.splice(0,b8.length,...b8);
+								L8.splice(0,b8.length,b8);
 							chan.buffer8 = m8.slice(m8.length-dl,dl);	
 						}			// Now dl and dr contain the correct offsets
 	  					for (let i=dl; i < m8.length; i++) L8[i] += m8[i-dl] * g;	
@@ -249,12 +248,12 @@ if (tracecount > 0) console.log("delaying ",dr," samples on right channel. Mono8
 						if (d < 0) {		// Apply delay to right channel
 							dr = d * -2;	// Invert delay and multiply by 2 for 16kHz audio
 							if (b16 != [])			// If there are samples in the delay buffer
-								R16.splice(0,b16.length,...b16);
+								R16.splice(0,b16.length,b16);
 							chan.buffer16 = m16.slice(m16.length-dr,dr);	// & copy final samples to delay buffer
 						} else {		// Apply delay to left channel
 							dl = d * 2;	// Multiply by 2 for 16kHz audio
 							if (b16 != [])			// Same as for right channel
-								L16.splice(0,b16.length,...b16);
+								L16.splice(0,b16.length,b16);
 							chan.buffer16 = m16.slice(m16.length-dl,dl);	
 						}			// Now dl and dr contain the correct offsets
 						for (let i=dl; i < m16.length; i++) L16[i] += m16[i-dl] * g;
