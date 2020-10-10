c4386902a2ce8c765b53c76247b5d844dcaa7822
sweatysock <mark@electricgt.co>
2020-10-10 11:35:47 +0100

    Now trying more aggressive buffer control measures to avoid hitting the stops

--- public/js/main.js
+++ public/js/main.js
@@ -1710,9 +1710,9 @@ function printReport() {
 	spkrBuffTrough = maxBuffSize;
 	deltaMax = 0;
 	deltaMin = 10000;
-	pitch = Math.round((maxBuffSize/2 - spkrBufferL.length)/500);	// pitch error is related inversely to buffer over/under middle
-	pitch = (pitch > 15)? 15 : pitch;
-	pitch = (pitch < -15)? -15 : pitch;
+	pitch = Math.round(Math.pow((maxBuffSize/2 - spkrBufferL.length),2)/250000);	// pitch error is related inversely to buffer over/under middle
+	pitch = (pitch > 12)? 12 : pitch;
+	pitch = (pitch < -12)? -12 : pitch;
 	adjMicPacketSize = micAudioPacketSize + pitch;			// pitch is adjusted to keep things flowing smoothly
 	enterState( idleState );					// Back to Idling
 }
