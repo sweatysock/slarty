e93177b7a31dc6f2c635fd78e37300c19b5f1df0
sweatysock <mark@electricgt.co>
2020-10-24 17:40:33 +0100

    Trying more aggressive venue compression...

--- public/js/main.js
+++ public/js/main.js
@@ -64,7 +64,7 @@ var mixOut = {								// Similar structures for the mix output
 var micIn = {								// and for microphone input
 	name 	: "Mic",
 	gain	: 0,
-	gainRate: 10,
+	gainRate: 100,
 	targetGain: 1,
 	ceiling : 1,
 	agc	: true,
@@ -77,7 +77,7 @@ var micIn = {								// and for microphone input
 var venue = {								// Similar structure for the venue channel
 	name 	: "Venue",
 	gain	: 0,
-	gainRate: 100,
+	gainRate: 10,
 	targetGain: 8,
 	ceiling : 1,
 	agc	: true,
