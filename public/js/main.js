bce52006615a8a41da1f4b602a9612d97c0a93ab
sweatysock <mark@electricgt.co>
2020-10-25 13:32:05 +0000

    High pass filter sounds better. Trying even more aggressive value. Timing downstream process much more...

--- public/js/main.js
+++ public/js/main.js
@@ -431,7 +431,7 @@ console.time("perf");
 console.timeEnd("perf");
 console.time("isStereo");
 		// 4. Adjust gain of final mix containing performer and group audio, and send to the speaker buffer
-		var obj;						// VAR MARK ?????
+		let obj;						// VAR MARK ?????
 		if (isStereo) {
 			let peakL = maxValue(mixL);			// Set gain according to loudest channel
 			let peakR = maxValue(mixR);
