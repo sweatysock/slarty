bba16c807ceb969b0ce756842fa2696e3ba3bb3c
sweatysock <mark@electricgt.co>
2020-10-26 16:58:51 +0000

    Investigating mic threshold total fail in chrome

--- public/js/main.js
+++ public/js/main.js
@@ -1029,7 +1029,6 @@ function talkoverOff() {						// Audio output system tries to get control again 
 		talkover--;
 	else	
 		mixOut.ceiling = 1;
-	}
 }
 
 var thresholdBands = [0.0001, 0.0002, 0.0003, 0.0005, 0.0007, 0.001, 0.002, 0.003, 0.005, 0.007, 0.01, 0.02, 0.03, 0.05, 0.07, 0.1, 0.2, 0.3, 0.5, 0.7, 1, 2];
