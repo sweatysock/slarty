e09b6050ea3fe3b4c61834abc2bcc90100a0a6c8
sweatysock <mark@electricgt.co>
2020-10-27 20:25:06 +0000

    Still adjusting and testing strong gating to eliminate feedback issues. Stretching/shrinking data on hold as it generates glitches... need to investigate more.

--- public/js/main.js
+++ public/js/main.js
@@ -1253,8 +1253,8 @@ function processAudio(e) {						// Main processing loop
 		thresholdBuffer.unshift( maxL );			// add to start of dynamic threshold queue
 		let s = echoTest.sampleDelay - 0;			// start of threshold window
 		let e = echoTest.sampleDelay + 0;			// end of threshold window
-		micIn.threshold = maxValue( thresholdBuffer		// Apply most aggressive threshold near current +/-w chunks
-			.slice(s,e)) * echoTest.factor * mixOut.gain;	// multiply by factor and mixOutGain 
+		micIn.threshold = maxValue( thresholdBuffer[s]		// Apply most aggressive threshold near current +/-w chunks
+			* echoTest.factor * mixOut.gain;	// multiply by factor and mixOutGain 
 		if (micIn.threshold > 0.2) micIn.threshold = 1.2;	// Values from mic can be > 1!!
 		thresholdBuffer.pop();					// Remove oldest threshold buffer value
 	} else micIn.threshold = 0;					// No echo risk so no threshold needed
