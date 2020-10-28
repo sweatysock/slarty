bfb325f7df1dd94a2229cc1f4773268cfc0748b0
sweatysock <mark@electricgt.co>
2020-10-28 00:24:46 +0000

    Need to fully test mobile speaker audio gating, NaN issue, and group member muting, but now adding invisible video play to stop screen sleeping

--- public/js/main.js
+++ public/js/main.js
@@ -1278,7 +1278,7 @@ if (isNaN(outAudioV[i])) tracef("NANV");
 		micIn.threshold = maxValue( thresholdBuffer		// Apply most aggressive threshold near current +/-w chunks
 			.slice(s,e)) * echoTest.factor * mixOut.gain;	// multiply by factor and mixOutGain 
 		if (micIn.threshold > 0.1) micIn.threshold = 0.8;	// Values from mic can be > 1!!
-		if (micIn.threshold > 0.2) micIn.threshold = 1.0;	// Values from mic can be > 1!!
+		if (micIn.threshold > 0.2) micIn.threshold = 0.9;	// Values from mic can be > 1!!
 		if (micIn.threshold > 0.4) micIn.threshold = 1.2;	// Values from mic can be > 1!!
 		thresholdBuffer.pop();					// Remove oldest threshold buffer value
 	} else micIn.threshold = 0;					// No echo risk so no threshold needed
