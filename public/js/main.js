7c2b94207239be5f6fd0afc62b4625fcf4b054ad
sweatysock <mark@electricgt.co>
2020-10-30 17:03:42 +0000

    Added blocking feature to stop over sensitive mic injecting incfreasing sound before threshold has ramped

--- public/js/main.js
+++ public/js/main.js
@@ -1288,10 +1288,10 @@ trace2("CUT ",peak,">",micIn.threshold);
 		let tempThresh;						// Adjusted threshold level 
 		tempThresh = maxValue( thresholdBuffer			// Apply most aggressive threshold near current +/-w chunks
 			.slice(s,e)) * echoTest.factor * mixOut.gain;	// multiply by factor and mixOutGain 
-		let gap = 0.8/echoTest.factor;				// The factor keeps threshold high stopping feedback. 
+		let gap = 0.9/echoTest.factor;				// The factor keeps threshold high stopping feedback. 
 		if (tempThresh > 0.5) tempThresh = 1.2;			// Above output of 0.5 there's no chance of getting control without muting output
-		else if (tempThresh > gap) tempThresh = 0.8;		// Between gap and 0.5 there is a chance of interrupting if you shout or clap
-		else if (tempThresh > gap*0.5) tempThresh = 0.5;	// and for slightly lower levels there is a slightly more tolerant gap kept open
+		else if (tempThresh > gap) tempThresh = 0.9;		// Between gap and 0.5 there is a chance of interrupting if you shout or clap
+		else if (tempThresh > gap*0.6) tempThresh = 0.6;	// and for slightly lower levels there is a slightly more tolerant gap kept open
 		thresholdBuffer.pop();					// Remove oldest threshold buffer value
 		if (blocked == 0) {  					// If blocked flag is reset we have passed a silent period and we need to watch for raising output
 			if (thresholdBuffer[0] > thresholdBuffer[1]) {	// If our output level is climbing thre's a risk of feedback due to mic over amplification
