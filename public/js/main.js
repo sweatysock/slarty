d8a92b4e9c45888e7eee4d1c510b3c3791857204
sweatysock <mark@electricgt.co>
2020-10-31 19:24:48 +0000

    Convoving input and output peak buffers to find delay and ratio in order to dynamically set threshold

--- public/js/main.js
+++ public/js/main.js
@@ -1301,8 +1301,8 @@ function processAudio(e) {						// Main processing loop
 		let min1 = 100; max = 0, min2 = 100;			// Find the first minimum, the maximum, and the second minimum
 		let min1p = 0, maxp = 0, min2p = 0;			// also note the positions where they occur in the conv array 
 		for (let j=0; j<conv.length; j++) {
-			if (conv[j] < min1) {
-				min1 = conv[j];
+			if ((conv[j] < min1) && (maxp <= min1p)) {	// If this is a minimum and the max is still behind us
+				min1 = conv[j];				// this can be a new first minimum
 				min1p = j;
 			}
 			if (conv[j] > max) {
