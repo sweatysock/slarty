81af5b6c5ebc027956bbb053346d9fb3d5da200d
sweatysock <mark@electricgt.co>
2020-11-02 14:17:44 +0000

    Convoving input and output peak buffers to find delay and ratio in order to dynamically set threshold

--- public/js/main.js
+++ public/js/main.js
@@ -1315,6 +1315,7 @@ trace2("quick test");
 	for (let i=0;i<(micPeaks.length-del);i++)			// Add up all the input channel peaks
 		sumMP += micPeaks[i];					// that may have been influenced by output
 	let aLot = 0.2 * (outputPeaks.length - del);			// An amount of sound that is non-trivial
+trace2("ready");
 	if ((sumOP > aLot) && (sumMP < (aLot/4))) {	 		// If our output is significant and our input small
 trace2("ECHO risk gone! mic & out:");
 let st="";
