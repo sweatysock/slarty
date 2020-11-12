ac574673bf0947dacde058980e9e5f831873b36d
sweatysock <mark@electricgt.co>
2020-11-12 18:29:35 +0000

    fixing venue audio...

--- public/js/main.js
+++ public/js/main.js
@@ -280,16 +280,15 @@ socketIO.on('d', function (data) {
 			while (packetBuf.length) {			// Scan the packet buffer for the packet with this sequence
 				let p = packetBuf.shift();		// Remove the oldest packet from the buffer until s is found
 				if (p.sequence == s) {			// We have found the right sequence number
-//					a8 = p.audio.mono8;		// Get our MSRE blocks from packet buffer
-//					a16 = p.audio.mono16;	
+					a8 = p.audio.mono8;		// Get our MSRE blocks from packet buffer
+					a16 = p.audio.mono16;	
 					break;				// Packet found so stop scanning the packet buffer. 
 				}
 			}
 		}
 		let audio = zipson.parse(vData.audio);			// Uncompress venue audio
 		let v8 = audio.mono8, v16 = audio.mono16;		// Shortcuts to the venue MSRE data blocks
-console.log(v8);
-console.log(v16);
+		v8 = a8; v16 = a16; a8 = []; a16 = [];
 		if ((v8.length > 0) && (!venue.muted)) {		// If there is venue audio & not muted, it will need processing
 			let sr = 8000;					// Minimum sample rate of 8kHz
 			let gn = venue.gain / venueSize;		// Gain adjusts for fader setting and venue size most importantly
